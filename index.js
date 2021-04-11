0faba0adebb1430236242ca2be8c111ae0c8796f
antremis <57432501+antremis@users.noreply.github.com>
2021-04-11 19:00:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
+function min(a, b){
+    if(a < b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+
 function expandWindow(){
-    document.getElementById("btnStart").style.transitionDuration = "2s"
-    var windowHeight = document.getElementById("main-program").clientHeight
+    document.getElementById("btnStart").style.transitionDuration = "2s";
     document.getElementById("btnStart").style.opacity = 0;
+    document.getElementById("btnStart").style.fontSize = 0;
+    document.getElementById("main-program").style.borderTop = "2px solid black";
+    expandWindowWidth()
+    window.setTimeout(expandWindowHeight, 2000)
+}
+
+function expandWindowWidth(){
+    var windowWidth = document.getElementById("main-program").clientWidth;
+    var increaseWindow = setInterval(function() {
+        if(windowWidth >= min((window.innerWidth)*0.9, 1200)){
+            windowWidth = min((window.innerWidth)*0.9, 1200);
+            document.getElementById("main-program").style.width = windowWidth + "px";
+            clearInterval(increaseWindow);
+        }
+        else{
+            windowWidth += 5;
+            document.getElementById("main-program").style.width = windowWidth + "px";
+        }
+    }, 10);
+    window.setTimeout(function() {
+        document.getElementById("btnStart").style.display = "none";
+    }, 2000);
+}
+
+function expandWindowHeight(){
+    var windowHeight = document.getElementById("main-program").clientHeight
+    document.getElementById("main-program").style.borderLeft = "2px solid black";
+    document.getElementById("main-program").style.borderRight = "2px solid black";
+    window.setTimeout(function(){
+        document.getElementById("main-program").style.borderBottom = "2px solid black";
+    }, 2200)
     var increaseWindow = setInterval(function() {
         if(windowHeight >= (window.innerHeight)*0.8){
             windowHeight = (window.innerHeight)*0.8;
@@ -10,13 +49,9 @@ function expandWindow(){
         }
         else{
             windowHeight += 20;
-            window.scrollY += 20;
             document.getElementById("main-program").style.height = windowHeight + "px";
         }
     }, 20);
-    window.setTimeout(function() {
-        document.getElementById("btnStart").style.display = "none";
-    }, 2000);
 }
 
 function test(){
